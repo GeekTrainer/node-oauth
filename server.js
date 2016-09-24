743eda21b0d051ebfcda265a011c19f21a6d8199
Christopher Harrison <chrhar@microsoft.com>
2016-09-23 20:39:16 -0700

    initial server create

---
+++ server.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const builder = require('botbuilder');
+const restify = require('restify');
+require('dotenv').load();
+
+const connector = new builder.ChatConnector();
+const bot = new builder.UniversalBot(connector);
+
+bot.dialog('login', require('./dialogs/oauth.js'));
+
+const server = restify.createServer();
+server.use(restify.queryParser());
+
+server.post('/api/messages', connector.listen());
+server.get('/oauth', (req, res) => {
+    res.send(200, `Paste this code into the bot: ${req.query.code}`);
+});
+
+server.listen(process.env.PORT || process.env.port, () => {
+
+});
